fd34ac0d3d18a37abf4911e18036cc499952b8ba
muhamedshijas <shijushijas157@gmail.com>
2023-09-22 09:34:29 +0530

    model created

---
+++ script.js
@@ -0,0 +1,45 @@
+const quizData=[
+    {
+        qustion:"Who is your protector?",
+        a:"you",
+        b:"parents",
+        c:"lover",
+        d:"none of these",
+        correct:"b"
+    },{
+        qustion:"Who is your protector?",
+        a:"you",
+        b:"parents",
+        c:"lover",
+        d:"none of these",
+        correct:"b"
+    },{
+        qustion:"Who is your protector?",
+        a:"you",
+        b:"parents",
+        c:"lover",
+        d:"none of these",
+        correct:"b"
+    },{
+        qustion:"Who is your protector?",
+        a:"you",
+        b:"parents",
+        c:"lover",
+        d:"none of these",
+        correct:"b"
+    },{
+        qustion:"Who is your protector?",
+        a:"you",
+        b:"parents",
+        c:"lover",
+        d:"none of these",
+        correct:"b"
+    },{
+        qustion:"Who is your protector?",
+        a:"you",
+        b:"parents",
+        c:"lover",
+        d:"none of these",
+        correct:"b"
+    },
+]
