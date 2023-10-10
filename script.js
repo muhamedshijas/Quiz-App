f839cf6c4bf41e35ace6380591e0ca5ba1033e46
muhamedshijas <shijushijas157@gmail.com>
2023-10-10 10:57:17 +0530

    fully updated

--- script.js
+++ script.js
@@ -81,19 +81,58 @@ const quizData = [
     }
 ];
 
-const questionEl=document.getElementById('question')
-const a_text=document.getElementById("a_text")
-const b_text=document.getElementById("b_text")
-const c_text=document.getElementById("c_text")
-const d_text=document.getElementById("d_text")
-let currentQuiz=0;
+const quiz = document.getElementById('quiz')
+const answerELS = document.querySelectorAll(".answer")
+
+const questionEl = document.getElementById('question')
+const a_text = document.getElementById("a_text")
+const b_text = document.getElementById("b_text")
+const c_text = document.getElementById("c_text")
+const d_text = document.getElementById("d_text")
+const submitBtn = document.getElementById('submit')
+let currentQuiz = 0;
+let score = 0
 loadQuiz();
-function loadQuiz(){
+function loadQuiz() {
+    deSelect()
     console.log("huuu")
-    const currentQuizData=quizData[currentQuiz]
-    questionEl.innerHTML=currentQuizData.question
-    a_text.innerText=currentQuizData.a
-    b_text.innerText=currentQuizData.b
-    c_text.innerText=currentQuizData.c
-    d_text.innerText=currentQuizData.d
+    const currentQuizData = quizData[currentQuiz]
+    questionEl.innerHTML = currentQuizData.question
+    a_text.innerText = currentQuizData.a
+    b_text.innerText = currentQuizData.b
+    c_text.innerText = currentQuizData.c
+    d_text.innerText = currentQuizData.d
 }
+
+function getSelected() {
+    let answer = undefined
+    answerELS.forEach(answerEL => {
+        if (answerEL.checked) {
+            answer = answerEL.id
+        }
+    })
+    return answer
+}
+function deSelect(){
+    answerELS.forEach((answerEL)=>{
+        answerEL.checked=false
+    })
+}
+submitBtn.addEventListener('click', () => {
+    const answer = getSelected()
+    if (answer) {
+        if(answer===quizData[currentQuiz].correct){
+            score++
+        }
+        currentQuiz++
+        if (currentQuiz < quizData.length) {
+
+            loadQuiz()
+        } else {
+          quiz.innerHTML=`You answerd Correctly<h2>${score}/${quizData.length} questions</h2>`
+        }
+    }
+
+
+})
+
